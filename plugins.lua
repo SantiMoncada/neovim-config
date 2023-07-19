455a50281aabc9a3f2466b6211fff1ce657ecd7c
Santiago Moncada <santiago.m@landbot.io>
2023-07-19 18:26:22 +0200

    added lsp ts and go

--- plugins.lua
+++ plugins.lua
@@ -2,6 +2,21 @@ local plugins = {
   {
     "christoomey/vim-tmux-navigator",
     lazy = false,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+       "gopls",
+      },
+    },
+  },
+  {
+  "neovim/nvim-lspconfig",
+    config = function ()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end
   }
 }
 
